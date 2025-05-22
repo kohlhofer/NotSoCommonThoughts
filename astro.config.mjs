1c2b903ca01aacdc7b981bd74a981462422e5920
kohlhofer <3952+kohlhofer@users.noreply.github.com>
2025-05-22 09:19:40 -0400

    Add Astro integrations: - Added sitemap generation - Added gzip and brotli compression

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,29 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import compress from 'vite-plugin-compression';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://notsocommonthoughts.com',
-  integrations: [tailwind(), mdx()],
+  integrations: [tailwind(), mdx(), sitemap()],
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
       wrap: true
     }
+  },
+  vite: {
+    plugins: [
+      compress({
+        algorithm: 'gzip',
+        ext: '.gz',
+      }),
+      compress({
+        algorithm: 'brotliCompress',
+        ext: '.br',
+      }),
+    ],
   }
 });
