bf6efe42b378b674411c794f98e737b0d2cd86f4
kohlhofer <3952+kohlhofer@users.noreply.github.com>
2025-05-23 14:18:12 -0400

    Add Google Analytics tracking

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,14 @@ import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import compress from 'vite-plugin-compression';
+import { analytics } from 'astro-analytics';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://notsocommonthoughts.com',
-  integrations: [tailwind(), mdx(), sitemap()],
+  integrations: [tailwind(), mdx(), sitemap(), analytics({
+    measurementIds: ['G-DTND00MKLK']
+  })],
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
